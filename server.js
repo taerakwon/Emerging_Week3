dc00edc352fbb8792c4f468eb52a23209da9a90b
taerakwon <taerakwon@gmail.com>
2017-01-28 13:28:34 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,47 @@
+// Your server will live here
+// import connect
+let connect = require('connect');
+let app = connect(); // Now app is instance of connect server
+const port = 3000;
+// start listening on the port
+app.listen(port);
+console.log(`Server started at ${port}`);
+
+// ROUTING - Mounted our routes
+
+// Second route is '/hello' which is root of my website
+app.use('/hello', (req, res, next) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("Hello, World!");
+    next();
+});
+// req = request
+// res = response
+// next = next response in queue
+
+// First route is '/' which is root of my website
+app.use('/', (req, res, next) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("Welcome!");
+    next();
+});
+
+
+/*
+function logger(req, res, next){
+    console.log(req.method, req.url);
+    next();
+};
+
+function helloWorld(req, res, next){
+    res.setHeader('Content-Type','text/plain');
+    res.end('Hello World');
+};
+
+function goodbyeWorld(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Goodbye World');
+}
+
+app.use(logger);
+*/
